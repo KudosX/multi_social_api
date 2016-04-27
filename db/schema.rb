441f1379a5cf73d2190cf52ad57b236bfc0e25f1
KudosX <max@shitbrains.com>
2016-04-27 12:26:27 -0700

    fixed database error, inte vs. integer

--- db/schema.rb
+++ db/schema.rb
@@ -33,8 +33,8 @@ ActiveRecord::Schema.define(version: 20160419225001) do
     t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
+    t.integer  "current_sign_in_ip"
+    t.integer  "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
